09a097b5c7f2edbe8220c1e7fefd8ff47b492fc6
vanshhika <vinnypatel2000@gmail.com>
2024-04-22 21:10:35 -0400

    Modified GUI edits

--- userInterface.py
+++ userInterface.py
@@ -23,87 +23,231 @@ class Ui_MainWindow(object):
         self.window.show()
 
 
-    def setupUi(self, MainWindow): 
-        # Main selection window
-        # Create object for the current selection window
-        MainWindow.setObjectName("SelectionWindow")
-        MainWindow.resize(600, 300)
+    def setupUi(self, MainWindow):
+        # Set up the main window for selection with a fixed size and style sheet
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setFixedSize(1200, 800)
+        MainWindow.setStyleSheet("QMainWindow:{background-color:\"#795eff\"}")
+
+        # create a central widget and stylesheet
         self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setStyleSheet("background-color:\"#E3FEF7\"")
         self.centralwidget.setObjectName("centralwidget")
-        # Apply Background color:
-        
-        # Title Label:
-        self.titleLabel = QtWidgets.QLabel(self.centralwidget)
-        self.titleLabel.setGeometry(QtCore.QRect(200,5,300,40))
+
+        # create radio button 1 for Wikipedia/URL option and style it 
+        self.radioButton = QtWidgets.QRadioButton(self.centralwidget,  clicked = lambda: self.wikipedia_window())       
+        self.radioButton.setGeometry(QtCore.QRect(450, 340, 300, 40))
         font = QtGui.QFont()
-        font.setPointSize(24)
-        font.setBold(True)
-        self.titleLabel.setFont(font)
-        self.titleLabel.setStyleSheet("QLabel {color: #AED4E6; }")
-        
-        # Radio button if selection open Wikipedia page
-        self.radioButton = QtWidgets.QRadioButton(self.centralwidget, clicked = lambda: self.wikipedia_window())
-        self.radioButton.setGeometry(QtCore.QRect(50, 50, 300, 150))
-        font = QtGui.QFont()
-        font.setPointSize(15)
-        font.setBold(True)
-        font.setWeight(75)
+        font.setFamily("Times New Roman")
+        font.setPointSize(20)
+        font.setItalic(False)
         self.radioButton.setFont(font)
-        self.radioButton.setStyleSheet("")
+        self.radioButton.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.radioButton.setStyleSheet("font: 800 20pt \"Times New Roman\";\n" "color: rgb(19, 93, 102);")
         self.radioButton.setObjectName("radioButton")
-        self.radioButton.setStyleSheet("QRadioButton {color: #F4D1AE; }")
-
-        # Radio button if selection open text page
+        # create radio button 1 for Text option and style it 
         self.radioButton_2 = QtWidgets.QRadioButton(self.centralwidget, clicked = lambda: self.text_window())
-        self.radioButton_2.setGeometry(QtCore.QRect(250, 50, 300, 150))
+        self.radioButton_2.setGeometry(QtCore.QRect(450, 440, 300, 40))
         font = QtGui.QFont()
-        font.setPointSize(15)
-        font.setBold(True)
-        font.setWeight(75)
+        font.setFamily("Times New Roman")
+        font.setPointSize(20)
+        font.setItalic(False)
         self.radioButton_2.setFont(font)
+        self.radioButton_2.setStyleSheet("font: 800 20pt \"Times New Roman\";\n" "color: rgb(19, 93, 102);")
         self.radioButton_2.setObjectName("radioButton_2")
-        self.radioButton_2.setStyleSheet("QRadioButton {color: #F4D1AE; }")
 
-        # Set the text for the label
+        # create and setup ui elements with geometry, palettes for colors for first label
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(50, 55, 400, 55))
-        self.label.setStyleSheet("QLabel {color: #F4D1AE; }")
-        font = QtGui.QFont()
-        font.setPointSize(1)
-        font.setBold(True)
-        font.setWeight(75)
-        
+        self.label.setGeometry(QtCore.QRect(250, 200, 700, 50))
         palette = QtGui.QPalette()
         brush = QtGui.QBrush(QtGui.QColor(170, 0, 0))
         brush.setStyle(QtCore.Qt.SolidPattern)
         palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Button, brush)
+
+        # set other colors for the active states like text, button text, base, window
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67, 128))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.PlaceholderText, brush)
+
+        # set other colors for the inactive states like text, button text, base, window  
         brush = QtGui.QBrush(QtGui.QColor(170, 0, 0))
         brush.setStyle(QtCore.Qt.SolidPattern)
         palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Button, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67, 128))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.PlaceholderText, brush)
+
+        # set other colors for the disabled states like text, button text, base, window
         brush = QtGui.QBrush(QtGui.QColor(120, 120, 120))
         brush.setStyle(QtCore.Qt.SolidPattern)
         palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Button, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67, 128))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.PlaceholderText, brush)
+
+        # apply the palette to the label
         self.label.setPalette(palette)
+
+        # set font for label
         font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setItalic(True)
+        font.setFamily("Times New Roman")
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setItalic(False)
         self.label.setFont(font)
+        self.label.setStyleSheet("font: 700 20pt \"Times New Roman\";\n" "color: rgb(0, 60, 67);")
         self.label.setObjectName("label")
+
+        # create and setup ui elements with geometry, palettes for colors for first label
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(300, 80, 600, 60))
+        palette = QtGui.QPalette()
+
+        # set other colors for the active states like text, button text, base, window
+        brush = QtGui.QBrush(QtGui.QColor(170, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Button, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67, 128))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.PlaceholderText, brush)
+
+        # set other colors for the inactive states like text, button text, base, window
+        brush = QtGui.QBrush(QtGui.QColor(170, 0, 0))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Button, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67, 128))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.PlaceholderText, brush)
+
+        # set other colors for the disabled states like text, button text, base, window
+        brush = QtGui.QBrush(QtGui.QColor(120, 120, 120))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Button, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Base, brush)
+        brush = QtGui.QBrush(QtGui.QColor(227, 254, 247))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Window, brush)
+        brush = QtGui.QBrush(QtGui.QColor(0, 60, 67, 128))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.PlaceholderText, brush)
+        self.label_2.setPalette(palette)
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(30)
+        font.setItalic(False)
+        self.label_2.setFont(font)
+        self.label_2.setAutoFillBackground(False)
+        self.label_2.setStyleSheet("font: 800 30pt \"Times New Roman\";\n" "color: rgb(0, 60, 67);")
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+
+        # set central widget and status bar for the main window
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
+        # connect signals and slots and retranslate UI
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("SelectionWindow", "Selection Window"))
-        self.radioButton.setText(_translate("SelectionWindow", "WIKIPEDIA"))
-        self.radioButton_2.setText(_translate("SelectionWindow", "TEXT"))
-        self.label.setText(_translate("SelectionWindow", "Please select the type of summarization you would like to perform"))
-        self.titleLabel.setText(_translate("SelectionWindow", "Selection Window"))
+        # set title for the main window
+        MainWindow.setWindowTitle(_translate("MainWindow", "Text Summarization"))
+        # set text for the first radio button
+        self.radioButton.setText(_translate("MainWindow", "WIKIPEDIA/URL"))
+        # set text for the second radio button
+        self.radioButton_2.setText(_translate("MainWindow", "TEXT"))
+        # set text for first label
+        self.label.setText(_translate("MainWindow", "Please Select the option for Text Summarization:"))
+        # set text for second label
+        self.label_2.setText(_translate("MainWindow", "SELECTION WINDOW"))
 
 if __name__ == "__main__":
     import sys
